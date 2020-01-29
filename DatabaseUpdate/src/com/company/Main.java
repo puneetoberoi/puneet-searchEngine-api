a7ece555e90f292a6c4b878e7437470c38bb416a
puneet <jasibr4@gmail.com>
2020-01-28 19:27:11 -0800

    removed comments

--- DatabaseUpdate/src/com/company/Main.java
+++ DatabaseUpdate/src/com/company/Main.java
@@ -1,9 +1,5 @@
 package com.company;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
 import java.io.IOException;
 
 public class Main {
@@ -12,31 +8,5 @@ public class Main {
 	    new FirstDBUpdate(); //This is the first step if starting from scratch.
         new SecondDBUpdate();
 
-//        Document document;
-//        try {
-//            //Get Document object after parsing the html from given url.
-//            document = Jsoup.connect(
-//                    "https://www.w3schools.com/howto/howto_css_searchbar.asp")
-//                    .get();
-//
-//            //Get description from document object.
-//            String description =
-//                    document.select("meta[name=description]").get(0)
-//                            .attr("content");
-//            //Print description.
-//            System.out.println("Meta Description: " + description);
-//
-//            //Get keywords from document object.
-//            String keywords =
-//                    document.select("meta[name=keywords]").first()
-//                            .attr("content");
-//            //Print keywords.
-//            System.out.println("Meta Keyword : " + keywords);
-//            Elements hTags = document.select("h1, h2, h3, h4, h5, h6");
-//            Elements h1Tags = hTags.select("h1");
-//            System.out.println(h1Tags.text() + " wmk");
-//
-//        } catch (final Exception | Error ignored){
-//        }
     }
 }
